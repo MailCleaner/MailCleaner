5733df0ea01274596863d6fe6401ff43312abee2
Nuno Ricardo Almeida Oliveira <noliveira@fastnet.ch>
2017-10-13 07:11:40 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 14.6% (48 of 328 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/pt/

--- www/user/htdocs/lang/pt/texts.php
+++ www/user/htdocs/lang/pt/texts.php
@@ -1,1 +1,50 @@
 <?php
+$txt['NEWSLETTERSALLOW'] = "emissão";
+$txt['NEWSLETTERACCEPT'] = "Aceitar este boletim de informação";
+$txt['SPAM_WHITELIST_DISABLED'] = "Esta lista não està ativada para este domínio .";
+/* 
+ * generic buttons
+ */
+$txt['SUBMIT'] = "Enviar";
+$txt['CANCEL'] = "Cancelar";
+$txt['CLOSE'] = "Fechar";
+// old
+$txt['ADD'] = "adicionar";
+$txt['CONFIRM'] = "Confirmar";
+$txt['REFRESH'] = "Atualizar";
+$txt['SEARCH'] = "Pesquisar";
+$txt['SEND'] = "Enviar";
+// old
+$txt['CLEAR'] = "apagar";
+/*
+ * generic texts
+ */
+$txt['GB'] = "GB";
+$txt['MB'] = "MB";
+$txt['KB'] = "KB";
+$txt['BYTES'] = "bytes";
+$txt['DATEFORMAT'] = "_D__M__Y_";
+$txt['MONTHAB1'] = 'Jan.';
+$txt['MONTHAB2'] = 'Fev.';
+$txt['MONTHAB3'] = 'Mar.';
+$txt['MONTHAB4'] = 'Avr.';
+$txt['MONTHAB5'] = 'Mai.';
+$txt['MONTHAB6'] = 'Jun.';
+$txt['MONTHAB7'] = 'Jul.';
+$txt['MONTHAB8'] = 'Ago.';
+$txt['MONTHAB9'] = 'Sete.';
+$txt['MONTHAB10'] = 'Out.';
+$txt['MONTHAB11'] = 'Nov.';
+$txt['MONTHAB12'] = 'Dez.';
+$txt['MONTH1'] = 'Janeiro';
+$txt['MONTH2'] = 'Fevreiro';
+$txt['MONTH3'] = 'Março';
+$txt['MONTH4'] = 'Avril';
+$txt['MONTH5'] = 'Maio';
+$txt['MONTH6'] = 'Junho';
+$txt['MONTH7'] = 'Julho';
+$txt['MONTH8'] = 'Agosto';
+$txt['MONTH9'] = 'Setembro';
+$txt['MONTH10'] = 'Outubro';
+$txt['MONTH11'] = 'Novembro';
+$txt['MONTH12'] = 'Dezembro';
