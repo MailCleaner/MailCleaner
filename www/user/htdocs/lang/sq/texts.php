57a90cfc5d04c66e553d49b3faa215ca9a9a62b1
Mentor Reka <reka.mentor@gmail.com>
2017-10-14 08:02:18 +0000

    Translated using Weblate (Albanian)
    Currently translated at 17.6% (58 of 328 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/sq/

--- www/user/htdocs/lang/sq/texts.php
+++ www/user/htdocs/lang/sq/texts.php
@@ -1,1 +1,49 @@
 <?php
+$txt['NEWSLETTERSSHORTHELP'] = "Ketu mund ti menaxhoni newsletters.";
+$txt['NEWSLETTERSALLOW'] = "dergoni";
+$txt['NEWSLETTERACCEPT'] = "Pranoni kete newsletter";
+$txt['SPAM_WHITELIST_DISABLED'] = "Kjo liste nuk eshte e aktivuar per kete domain (Kontaktoni administratorin e juaj).";
+/* 
+ * generic buttons
+ */
+$txt['SUBMIT'] = "Dergo";
+$txt['CANCEL'] = "Anulo";
+$txt['CLOSE'] = "Mbyll";
+// old
+$txt['ADD'] = "shtoj";
+$txt['CONFIRM'] = "Pranoje";
+$txt['REFRESH'] = "Rifresko";
+$txt['SEARCH'] = "Kerko";
+$txt['SEND'] = "Dergo";
+// old
+$txt['CLEAR'] = "fshij";
+/*
+ * generic texts
+ */
+$txt['GB'] = "GB";
+$txt['MB'] = "MB";
+$txt['KB'] = "KB";
+$txt['BYTES'] = "bytes";
+$txt['MONTHAB1'] = 'Jan.';
+$txt['MONTHAB2'] = 'Shk.';
+$txt['MONTHAB3'] = 'Mar.';
+$txt['MONTHAB4'] = 'Pril.';
+$txt['MONTHAB5'] = 'Maj';
+$txt['MONTHAB6'] = 'Qershor';
+$txt['MONTHAB7'] = 'Korrik';
+$txt['MONTHAB8'] = 'Gusht';
+$txt['MONTHAB9'] = 'Sht.';
+$txt['MONTHAB10'] = 'Tet.';
+$txt['MONTHAB11'] = 'Nën.';
+$txt['MONTHAB12'] = 'Dhj.';
+$txt['MONTH1'] = 'Janar';
+$txt['MONTH2'] = 'Shkurt';
+$txt['MONTH4'] = 'Prill';
+$txt['MONTH5'] = 'Maj';
+$txt['MONTH6'] = 'Qershor';
+$txt['MONTH7'] = 'Korrik';
+$txt['MONTH8'] = 'Gusht';
+$txt['MONTH9'] = 'Shtator';
+$txt['MONTH10'] = 'Tetor';
+$txt['MONTH11'] = 'Nëntor';
+$txt['MONTH12'] = 'Dhjetor';
