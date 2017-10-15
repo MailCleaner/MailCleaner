c5f1dc648f35431ded84e07c0888da5c307d315c
Mentor Reka <reka.mentor@gmail.com>
2017-10-15 21:04:47 +0000

    Translated using Weblate (Albanian)
    Currently translated at 33.8% (111 of 328 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/sq/

--- www/user/htdocs/lang/sq/texts.php
+++ www/user/htdocs/lang/sq/texts.php
@@ -61,3 +61,59 @@ $txt['NAVSUPPORT'] = "Ndihmë";
 $txt['SELECTEDADDRESS'] = "Adresa e shfaqur";
 $txt['QUARANTINESUMMARY'] = "<strong> Në karantinë: </strong> __PARAM__ mesazhes";
 $txt['SEARCHSUMMARY'] = "<strong>Kerko :</strong> __PARAM__ mesazhes";
+$txt['ORDEREDBYPARAM'] = "rendit sipas __PARAM__";
+$txt['RESETSEARCH'] = "Shfaqni listën e plotë të karantinës";
+$txt['ODATE'] = "datë";
+$txt['OTIME'] = "kohë";
+$txt['ODESTINATION'] = "marrës";
+$txt['OSENDER'] = "dërgues";
+$txt['OSCORE'] = "pikë";
+$txt['OSUBJECT'] = "subjekt";
+//old
+$txt['CRITERIAS'] = "Rregullat e filtrit";
+$txt['FORCEMESSAGE'] = "Lëshoni mesazhin";
+$txt['MESSAGEFORCING'] = "Lirimi i mesazheve";
+$txt['ANALYSEREQUEST'] = "Kërkesa e rregullimit të filtrit";
+$txt['ANALYSEREQUESTV'] = "Kërkoni një rregullim të filtrit";
+$txt['SCORETEXT'] = "Pikë: __PARAM__";
+$txt['CLOSEWINDOW'] = "Mbylle dritarën";
+$txt['CURRENTPAGE'] = "Faqe __PARAM1__ nga __PARAM2__";
+$txt['PURGEINFOS'] = "<strong>Shpëlarje automatike pas: </strong> __PARAM__ ditë";
+$txt['DISPLAYEDINFOS'] = "<strong>Mesazhet shfaqen:</strong> __PARAM1__ ditët e fundit (<a href=\"__PARAM2__\"> Ndrysho ... </a>). ";
+$txt['DISPLAYEDINFOSS'] = "<strong>Mesazhet e shfaqura: </strong> __PARAM__ ditët e fundit. ";
+//old
+$txt['QUARANTINETITLE'] = "Lista e karantinës për __PARAM__";
+//old
+$txt['FILTER'] = "Filtër";
+//old
+$txt['ADVANCEDSEARCH'] = "kërkim i avancuar";
+//old
+$txt['SHOWEDMESSAGES'] = "mesazhe të shfaqura";
+//old
+$txt['COMINGFROM'] = "dërgues";
+$txt['DATE'] = "Datë";
+$txt['HOUR'] = "Orë";
+$txt['FROM'] = "Nga";
+$txt['TO'] = "Tek";
+$txt['SUBJECT'] = "Subjekt";
+$txt['FORCED'] = "I çliruar";
+$txt['ACTION'] = "Veprim";
+//old
+$txt['TOTALSPAMS'] = "Gjithsej: __PARAM__ spam(s)";
+//old
+$txt['FORTHEXLASTDAYS'] = "__PARAM__ ditët e fundit";
+$txt['PURGESELECTEDSPAMS'] = "Pastroni manualisht karantinën";
+//old
+$txt['HIDEFORCED'] = "Fshih mesazhet e lëshuara nga përdoruesit";
+//old
+$txt['FORCESPAM'] = "lironi mesazhin";
+//old
+$txt['ASKREASON'] = "rregullat e filtrimit";
+$txt['CONFSENDANALYSE'] = "Një kërkesë rregullimi filtri do të dërgohet në Qendrën e Analizës së MailCleaner; ai përfshin një kopje të mesazhit që është bllokuar gabimisht.";
+$txt['AREYOUSURE'] = "<span class=\"question\">A jeni i sigurt që dëshironi që ky mesazh të analizohet dhe që rregullimet e filtrit mund të aplikohen?</span>";
+$txt['ASKANALYSE'] = "Kërkesa e rregullimit të filtrit";
+$txt['PAGE'] = "Faqe";
+$txt['NEXTPAGE'] = "Tjetër";
+$txt['PREVIOUSPAGE'] = "I mëparshëm";
+//old
+$txt['GETREASONSTITLE'] = "Arsyet e filtrimit";
