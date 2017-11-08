6d4c25f5b8ebfa5a8e67af47be46c0d3f235417d
Michal Vano <vano@alfapro.sk>
2017-11-08 12:29:27 +0000

    Translated using Weblate (Slovak)
    Currently translated at 25.0% (4 of 16 strings)
    
    Translation: MailCleaner Anti Spam/Help texts
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/help-texts/sk/

--- www/user/htdocs/lang/sk/help_texts.php
+++ www/user/htdocs/lang/sk/help_texts.php
@@ -1,1 +1,5 @@
 <?php
+$htxt['MANUAL_FULL_NAME'] = 'mailcleaner_uzivatelska_prirucka';
+$htxt['MANUAL_FIRSTCONTACT_NAME'] = 'mailcleaner_rychly_prehlad.pdf';
+$htxt['MANUAL_STATS_NAME'] = 'mailcleaner_statistiky.pdf';
+$htxt['MANUAL_CONFIGURATION_NAME'] = 'mailcleaner_konfiguracia.pdf';
