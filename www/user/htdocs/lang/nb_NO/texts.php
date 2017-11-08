621e228718e40b9f1311c54fcca73659cb428549
anonymous <>
2017-11-08 16:39:15 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 0.3% (1 of 328 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/nb_NO/

--- www/user/htdocs/lang/nb_NO/texts.php
+++ www/user/htdocs/lang/nb_NO/texts.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * @license http://www.mailcleaner.net/open/licence_en.html Mailcleaner Public License
+ * @package mailcleaner
+ * @author Olivier Diserens
+ * @copyright 2006, Olivier Diserens
+ */
+### newsl
+$txt['NEWSLETTERMODULE'] = "Nyhetsbrev";
