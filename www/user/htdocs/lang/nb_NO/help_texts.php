b378ff9648765ff84da910e52d66b03826fd5c89
Allan Nordhøy <epost@anotheragency.no>
2017-11-11 18:14:29 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 50.0% (8 of 16 strings)
    
    Translation: MailCleaner Anti Spam/Help texts
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/help-texts/nb_NO/

--- www/user/htdocs/lang/nb_NO/help_texts.php
+++ www/user/htdocs/lang/nb_NO/help_texts.php
@@ -1,1 +1,9 @@
 <?php
+$htxt['MANUAL_FULL_NAME'] = 'mailcleaner_brukermanual.pdf';
+$htxt['MANUAL_FIRSTCONTACT_NAME'] = 'mailcleaner_hurtigguide.pdf';
+$htxt['MANUAL_GENERICCONCEPT_NAME'] = 'mailcleaner_hovedprinsipper.pdf';
+$htxt['MANUAL_GUI_NAME'] = 'mailcleaner_behandlingssenter.pdf';
+$htxt['MANUAL_QUARANTINE_NAME'] = 'mailcleaner_karantene.pdf';
+$htxt['MANUAL_STATS_NAME'] = 'mailcleaner_statistikk.pdf';
+$htxt['MANUAL_CONFIGURATION_NAME'] = 'mailcleaner_oppsett.pdf';
+$htxt['MANUAL_ERRORS_NAME'] = 'mailcleaner_filterfeil.pdf';
