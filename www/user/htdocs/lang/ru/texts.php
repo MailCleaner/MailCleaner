fbad7454b0431a4089ba56c8d2ed84514a84a268
Maksim Korzh <maksim.korzh@gmail.com>
2017-11-13 07:37:55 +0000

    Translated using Weblate (Russian)
    Currently translated at 30.1% (99 of 328 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/ru/

--- www/user/htdocs/lang/ru/texts.php
+++ www/user/htdocs/lang/ru/texts.php
@@ -1,2 +1,126 @@
 <?php
 $txt['NEWSLETTERSTOPIC'] = "Информационные бюллетени";
+$txt['NEWSLETTERACCEPT'] = "Одобрить эту рассылку";
+$txt['SPAM_WHITELIST_DISABLED'] = "Список не активирован для этого домена (обратитесь к администратору).";
+/* 
+ * generic buttons
+ */
+$txt['SUBMIT'] = "Отправить";
+$txt['CANCEL'] = "Отмена";
+$txt['CLOSE'] = "Закрыть";
+// old
+$txt['ADD'] = "Добавить";
+$txt['CONFIRM'] = "Подтвердить";
+$txt['REFRESH'] = "Обновить";
+$txt['SEARCH'] = "Поиск";
+$txt['SEND'] = "Отправить";
+// old
+$txt['CLEAR'] = "Очистить";
+/*
+ * generic texts
+ */
+$txt['GB'] = "Гб";
+$txt['MB'] = "Мб";
+$txt['KB'] = "Кб";
+$txt['BYTES'] = "байт";
+$txt['DATEFORMAT'] = "_Д__М__Г_";
+$txt['MONTHAB1'] = 'Янв.';
+$txt['MONTHAB2'] = 'Фев.';
+$txt['MONTHAB3'] = 'Мрт.';
+$txt['MONTHAB4'] = 'Апр.';
+$txt['MONTHAB5'] = 'Май';
+$txt['MONTHAB6'] = 'Июн';
+$txt['MONTHAB7'] = 'Июл';
+$txt['MONTHAB8'] = 'Авг.';
+$txt['MONTHAB9'] = 'Сен.';
+$txt['MONTHAB10'] = 'Окт.';
+$txt['MONTHAB11'] = 'Нбр.';
+$txt['MONTHAB12'] = 'Дек.';
+$txt['MONTH1'] = 'Январь';
+$txt['MONTH2'] = 'Февраль';
+$txt['MONTH3'] = 'Март';
+$txt['MONTH4'] = 'Апрель';
+$txt['MONTH5'] = 'Май';
+$txt['MONTH6'] = 'Июнь';
+$txt['MONTH7'] = 'Июль';
+$txt['MONTH8'] = 'Август';
+$txt['MONTH9'] = 'Сентябрь';
+$txt['MONTH10'] = 'Октябрь';
+$txt['MONTH11'] = 'Ноябрь';
+$txt['MONTH12'] = 'Декабрь';
+/*
+ * login page
+ */
+$txt['BADLOGIN'] = "Неверное имя пользователя или пароль.";
+$txt['SESSIONEXPIRED'] = "Время сеанса истекло.";
+$txt['LOGININFO'] = "Напоминание : Ваше имя пользователя и пароль совпадают с вашим аккаунтом электронной почты";
+$txt['USERNAME'] = "Имя пользователя";
+$txt['PASSWORD'] = "Пароль";
+//old
+$txt['ASKLOGIN'] = "Введите имя пользователя и пароль:";
+$txt['ORGANIZATIONUNIT'] = "Подразделение";
+$txt['FORGOTPASSWORD'] = "Забыли пароль?";
+$txt['PASSWORDRESET'] = "Пароль сброшен и отправлен на ваш почтовый адрес.";
+/*
+ * menu
+ */
+$txt['CONFIGURATIONMENU'] = "Настройки";
+$txt['HELPMENU'] = "Справка";
+$txt['QUARANTINEMENU'] = "Карантин";
+$txt['STATISTICSMENU'] = "Статистика";
+$txt['LOGOUT'] = "Выход";
+//old
+$txt['LOGGEDAS'] = "вы вошли как:";
+// old
+$txt['MAINADDRESS'] = "основной адрес";
+$txt['PARAMETERS'] = "Параметры";
+$txt['QUARANTINE'] = "Карантин";
+$txt['NAVSUPPORT'] = "Поддержка";
+/*
+ * quarantaine
+ */
+$txt['SELECTEDADDRESS'] = "Показывать для адреса";
+$txt['QUARANTINESUMMARY'] = "<strong>В карантине</strong>__PARAM__ messages";
+$txt['SEARCHSUMMARY'] = "<strong>Найти:</strong>__PARAM__ messages";
+$txt['ORDEREDBYPARAM'] = "отсортировано по __PARAM__";
+$txt['RESETSEARCH'] = "Показать все содержимое карантина";
+$txt['ODATE'] = "дата";
+$txt['OTIME'] = "время";
+$txt['ODESTINATION'] = "получатель";
+$txt['OSENDER'] = "отправитель";
+$txt['OSCORE'] = "оценка";
+$txt['OSUBJECT'] = "тема";
+//old
+$txt['CRITERIAS'] = "Правила фильтрации";
+$txt['FORCEMESSAGE'] = "Освободить сообщение";
+$txt['MESSAGEFORCING'] = "Сообщение освобождено";
+$txt['ANALYSEREQUEST'] = "Запрос на корректировку фильтра";
+$txt['ANALYSEREQUESTV'] = "Запросить корректировку фильтра";
+$txt['SCORETEXT'] = "Оценка: __PARAM__";
+$txt['CLOSEWINDOW'] = "Закрытие окна";
+$txt['CURRENTPAGE'] = "Страница __PARAM1__ из __PARAM2__";
+$txt['PURGEINFOS'] = "<strong>Автоматически удалить после:</strong> __PARAM__ дней";
+//old
+$txt['FILTER'] = "Фильтр";
+//old
+$txt['ADVANCEDSEARCH'] = "расширенный поиск";
+//old
+$txt['SHOWEDMESSAGES'] = "отображаемые сообщения";
+//old
+$txt['COMINGFROM'] = "отправитель";
+$txt['DATE'] = "Дата";
+$txt['HOUR'] = "Время";
+$txt['FROM'] = "От";
+$txt['TO'] = "Кому";
+$txt['SUBJECT'] = "Тема";
+$txt['FORCED'] = "Освобождено";
+$txt['ACTION'] = "Действие";
+//old
+$txt['TOTALSPAMS'] = "Всего: __PARAM__ спам";
+//old
+$txt['FORTHEXLASTDAYS'] = "__PARAM__ последних дней";
+/*
+ * preview panel
+ */
+$txt['INFORMATIONSABOUTMSG'] = "Содержание сообщения";
+$txt['RECORDALREADYEXISTS'] = "Этот адрес уже содержится в списке.";
