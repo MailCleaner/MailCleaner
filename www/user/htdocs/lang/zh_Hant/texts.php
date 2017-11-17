ddeb52292c3d508ffcadf555dc8275ba40998705
林楊竣 <s910092000@gmail.com>
2017-11-17 02:50:26 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 13.7% (45 of 328 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/zh_Hant/

--- www/user/htdocs/lang/zh_Hant/texts.php
+++ www/user/htdocs/lang/zh_Hant/texts.php
@@ -1,1 +1,61 @@
 <?php
+/**
+ * @license http://www.mailcleaner.net/open/licence_en.html Mailcleaner Public License
+ * @package mailcleaner
+ * @author Olivier Diserens
+ * @copyright 2006, Olivier Diserens
+ */
+### newsl
+$txt['NEWSLETTERMODULE'] = "通訊";
+$txt['NEWSLETTERSTOPIC'] = "簡訊";
+$txt['NEWSLETTERSTOPICTITLE'] = "簡訊";
+$txt['NEWSLETTERSSHORTHELP'] = "在這裡你可以管理你的通訊。";
+$txt['NEWSLETTERSALLOW'] = "交付";
+$txt['NEWSLETTERACCEPT'] = "接受本通訊";
+$txt['SPAM_WHITELIST_DISABLED'] = "該域名未激活（請聯繫您的管理員）。";
+/* 
+ * generic buttons
+ */
+$txt['SUBMIT'] = "提交";
+$txt['CANCEL'] = "取消";
+$txt['CLOSE'] = "關";
+// old
+$txt['ADD'] = "加";
+$txt['CONFIRM'] = "確認";
+$txt['REFRESH'] = "刷新";
+$txt['SEARCH'] = "搜索";
+$txt['SEND'] = "發送";
+// old
+$txt['CLEAR'] = "明確";
+/*
+ * generic texts
+ */
+$txt['GB'] = "GB";
+$txt['MB'] = "MB";
+$txt['KB'] = "KB";
+$txt['BYTES'] = "bytes";
+$txt['DATEFORMAT'] = "_D__M__Y_";
+$txt['MONTHAB1'] = 'Jan.';
+$txt['MONTHAB2'] = 'Feb.';
+$txt['MONTHAB3'] = 'Mar.';
+$txt['MONTHAB4'] = 'Apr.';
+$txt['MONTHAB5'] = 'May';
+$txt['MONTHAB6'] = 'June';
+$txt['MONTHAB7'] = 'July';
+$txt['MONTHAB8'] = 'Aug.';
+$txt['MONTHAB9'] = 'Sept.';
+$txt['MONTHAB10'] = 'Oct.';
+$txt['MONTHAB11'] = 'Nov.';
+$txt['MONTHAB12'] = 'Dec.';
+$txt['MONTH1'] = 'January';
+$txt['MONTH2'] = 'February';
+$txt['MONTH3'] = 'March';
+$txt['MONTH4'] = 'April';
+$txt['MONTH5'] = 'May';
+$txt['MONTH6'] = 'June';
+$txt['MONTH7'] = 'July';
+$txt['MONTH8'] = 'August';
+$txt['MONTH9'] = 'September';
+$txt['MONTH10'] = 'October';
+$txt['MONTH11'] = 'November';
+$txt['MONTH12'] = 'December';
