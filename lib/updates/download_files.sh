2a0163f604ea6fb36830c8f4ae79b5bc109f70d5
Pascal Rolle <squale@gmail.com>
2018-04-06 12:18:23 +0200

    Rely on changes made in dbs.md5 to check for updates

--- lib/updates/download_files.sh
+++ lib/updates/download_files.sh
@@ -59,7 +59,11 @@ function downloadfile {
 	mkdir -p $subDir
     fi
 
-    getfileres=`timeout 8m scp -q -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -C mcscp@cvs.mailcleaner.net:/$3/${file} $2$1 &>> $LOGFILE`
+    if [ -f $2$1 ]; then
+	rm $2$1 &>> $LOGFILE
+    fi
+
+    getfileres=`timeout 8m scp -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -C mcscp@cvs.mailcleaner.net:/$3/${file} $2$1 &>> $LOGFILE`
 
     if [ "$getfileres" = "" ]; then
   	log "$2 - $file successfully downloaded"
@@ -86,70 +90,69 @@ function downloadDatas {
     fi
 
     cd $1
-    
-    # For removed file: save local md5 file as old
-    OLDMD5FILE=${MD5FILE}.old
-    mv $MD5FILE ${OLDMD5FILE}
+   
+    # If no dbs.md5 was there, it is the first time we download this file
+    FIRSTTIME=1
 
-    scpres=`scp -q -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no mcscp@cvs.mailcleaner.net:/${2}/dbs.md5 $MD5FILE`
+    # If a dbs.md5 was there, we save it to know which files to update/delete according to differences with the new dbs.md5
+    if [ -e ${MD5FILE} ]; then
+	FIRSTTIME=0
+	OLDMD5FILE=${MD5FILE}.old
+	mv $MD5FILE ${OLDMD5FILE}
+    else
+	log "$2 - no previous dbs.md5"
+    fi
 
+    # Downloading the new dbs.md5
+    scpres=`scp -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no mcscp@cvs.mailcleaner.net:/${2}/dbs.md5 $MD5FILE &>> $LOGFILE`
     if [ ! -f $MD5FILE ]; then
 	log "$2 - Could not fetch md5 file"
 	if [ "$6" != "noexit" ]; then
                 exit 0
-            else
+        else
                 return 0
         fi
     fi
 
-    md5check=`/usr/bin/md5sum -c $MD5FILE 2>&1 | grep -i 'FAILED'`
-    if [ "$md5check" = "" ]; then
-	md5check=`/usr/bin/md5sum -c $MD5FILE 2>&1 | grep -i '.CHEC'`
-	if [ "$md5check" = "" ]; then
-	    log "$2 - datas are up-to-date"
-	    if [ "$6" != "noexit" ]; then
-		exit 0
-	    else
-		return 0
-	    fi
+    # For each file in the new dbs.md5, we download the file if this is needed
+    for file in $(cat ${MD5FILE} | cut -d " " -f3); do
+    	if [ $FIRSTTIME -eq 1 ]; then
+		downloadfile $file $1 $2 $4
+		log "$2 - $file downloaded"
+	else
+		log "$2 - getting hashes"
+		OLDHASH=$(grep -E "\b$file\b" ${OLDMD5FILE} |cut -d " " -f1)
+		HASH=$(grep -E "\b$file\b" ${MD5FILE} |cut -d " " -f1)
+
+		# New file
+		if [ "$OLDHASH" == "" ]; then
+			downloadfile $file $1 $2 $4
+			log "$2 - $file downloaded"
+
+		# Updated file
+		elif [ "$OLDHASH" != "$HASH" ]; then
+			downloadfile $file $1 $2 $4
+			log "$2 - $file downloaded"
+		else
+
+		# File didnt change
+			log "$2 - $file doesnt need to be downloading"
+		fi
 	fi
+    done
+
+    # Check for removed files (without removing custom files)
+    if [ $FIRSTTIME -ne 1 ]; then
+	for i in $(cat ${OLDMD5FILE} | cut -d " " -f3); do
+        	res=$(grep -E "\b$i\b" ${MD5FILE})
+	        if [ "$res" == "" ]; then
+        	        rm $i >> $LOGFILE 2>&1
+                	log "$2 - Removed file $i"
+	        fi
+	done
     fi
 
-    OLDIFS=$IFS
-    IFS=$'\n'
-
-    ## check for removed files (without removing custom files)
-    for i in $(cat ${OLDMD5FILE} | cut -d " " -f3); do
-        res=$(grep -E "\b$i\b" ${MD5FILE})
-        if [ "$res" == "" ]; then
-                rm $i >> /dev/null 2>&1
-                log "$2 - Removed file $i"
-        fi
-    done
-    rm ${OLDMD5FILE} >> /dev/null 2>&1
-
-    ## check for missing files
-    misres=`echo "$md5check" | grep -i "open or read"`
-    for l in $misres; do
-	misfile=`echo $l | cut -d':' -f1`
-	log "$2 - Missing file $misfile"
-	downloadfile $misfile $1 $2 $4
-    done
-
-    ## check for outdated files
-    outres=`echo "$md5check" | grep -i 'FAILED$'`
-    for l in $outres; do
-	outfile=`echo $l | cut -d":" -f1`
-	log "$2 - File: $outfile is out-dated"
-	downloadfile $outfile $1 $2 $4
-    done
-
-    ## check for outdated files
-    outres=`echo "$md5check" | grep -i '?CHEC$'`
-    for l in $outres; do
-	outfile=`echo $l | cut -d":" -f1`
-	log "$2 - File: $outfile is out-dated"
-	downloadfile $outfile $1 $2 $4
-    done
-
+    if [ $FIRSTTIME -ne 1 ]; then
+	rm ${OLDMD5FILE} >> $LOGFILE 2>&1
+    fi
 }
