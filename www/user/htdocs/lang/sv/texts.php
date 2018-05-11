1f88049ebd4fe8aef5eaed1c01fc8e7b11b8214f
Sebastian Rasmussen <sebras@gmail.com>
2018-05-11 13:39:54 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (332 of 332 strings)
    
    Translation: MailCleaner Anti Spam/User interface
    Translate-URL: https://hosted.weblate.org/projects/mailcleaner/user-interface/sv/

--- www/user/htdocs/lang/sv/texts.php
+++ www/user/htdocs/lang/sv/texts.php
@@ -12,21 +12,21 @@ $txt['NEWSLETTERSTOPICTITLE'] = "Nyhetsbrev";
 $txt['NEWSLETTERSSHORTHELP'] = "Här kan du hantera dina nyhetsbrev.";
 $txt['NEWSLETTERSALLOW'] = "leverera";
 $txt['NEWSLETTERACCEPT'] = "Acceptera detta nyhetsbrev";
-$txt['SPAM_WHITELIST_DISABLED'] = "The list is not activated for this domain (Contact your administrator).";
+$txt['SPAM_WHITELIST_DISABLED'] = "Listan är inte aktiverad för denna domän (kontakta din administratör).";
 /* 
  * generic buttons
  */
 $txt['SUBMIT'] = "Submit";
-$txt['CANCEL'] = "Cancel";
-$txt['CLOSE'] = "Close";
+$txt['CANCEL'] = "Avbryt";
+$txt['CLOSE'] = "Stäng";
 // old
-$txt['ADD'] = "add";
-$txt['CONFIRM'] = "Confirm";
-$txt['REFRESH'] = "Refresh";
-$txt['SEARCH'] = "Search";
+$txt['ADD'] = "lägg till";
+$txt['CONFIRM'] = "Bekräfta";
+$txt['REFRESH'] = "Uppdatera";
+$txt['SEARCH'] = "Sök";
 $txt['SEND'] = "Send";
 // old
-$txt['CLEAR'] = "clear";
+$txt['CLEAR'] = "rensa";
 /*
  * generic texts
  */
@@ -62,226 +62,226 @@ $txt['MONTH12'] = 'December';
 /*
  * login page
  */
-$txt['BADLOGIN'] = "Incorrect user name or password.";
-$txt['SESSIONEXPIRED'] = "Session has expired.";
-$txt['LOGININFO'] = "Reminder : Your user name and password are the same as your email account";
-$txt['USERNAME'] = "User name";
+$txt['BADLOGIN'] = "Felaktigt användarnamn eller lösenord.";
+$txt['SESSIONEXPIRED'] = "Sessionen har gått ut.";
+$txt['LOGININFO'] = "Påminnelse: Ditt användarnamn och lösenord är detsamma som ditt e-postkonto";
+$txt['USERNAME'] = "Användarnamn";
 $txt['PASSWORD'] = "Password";
 //old
-$txt['ASKLOGIN'] = "Please enter your user name and password:";
-$txt['ORGANIZATIONUNIT'] = "Organization unit";
-$txt['FORGOTPASSWORD'] = "Lost password ?";
-$txt['PASSWORDRESET'] = "The password has been reseted. It has been sent to your mail address.";
+$txt['ASKLOGIN'] = "Ange ditt användarnamn och lösenord:";
+$txt['ORGANIZATIONUNIT'] = "Organisationsenhet";
+$txt['FORGOTPASSWORD'] = "Glömt lösenordet?";
+$txt['PASSWORDRESET'] = "Lösenordet har återställts. Det har skickats till din e-postadress.";
 /*
  * menu
  */
 $txt['CONFIGURATIONMENU'] = "Configuration";
 $txt['HELPMENU'] = "Help";
-$txt['QUARANTINEMENU'] = "Quarantine";
-$txt['STATISTICSMENU'] = "Statistics";
-$txt['LOGOUT'] = "Logout";
+$txt['QUARANTINEMENU'] = "Karantän";
+$txt['STATISTICSMENU'] = "Statistik";
+$txt['LOGOUT'] = "Logga ut";
 //old
-$txt['LOGGEDAS'] = "you are logged in as:";
+$txt['LOGGEDAS'] = "du är inloggad som:";
 // old
-$txt['MAINADDRESS'] = "main address";
-$txt['PARAMETERS'] = "Settings";
-$txt['QUARANTINE'] = "Quarantine";
+$txt['MAINADDRESS'] = "huvudadress";
+$txt['PARAMETERS'] = "Inställningar";
+$txt['QUARANTINE'] = "Karantän";
 $txt['NAVSUPPORT'] = "Support";
 /*
  * quarantaine
  */
-$txt['SELECTEDADDRESS'] = "Address displayed";
-$txt['QUARANTINESUMMARY'] = "<strong>In quarantine:</strong> __PARAM__ messages";
-$txt['SEARCHSUMMARY'] = "<strong>Search:</strong> __PARAM__ messages";
-$txt['ORDEREDBYPARAM'] = "sorted by __PARAM__";
-$txt['RESETSEARCH'] = "Display the full quarantine list";
+$txt['SELECTEDADDRESS'] = "Adress visad";
+$txt['QUARANTINESUMMARY'] = "<strong>I karantän:</strong> __PARAM__ meddelanden";
+$txt['SEARCHSUMMARY'] = "<strong>Sök:</strong> __PARAM__ meddelanden";
+$txt['ORDEREDBYPARAM'] = "sorterade efter __PARAM__";
+$txt['RESETSEARCH'] = "Visa den fullständiga karantänlistan";
 $txt['ODATE'] = "date";
-$txt['OTIME'] = "time";
+$txt['OTIME'] = "tid";
 $txt['OSCORE'] = "score";
-$txt['ODESTINATION'] = "recipient";
-$txt['OSENDER'] = "sender";
+$txt['ODESTINATION'] = "mottagare";
+$txt['OSENDER'] = "avsändare";
 $txt['OSCORE'] = "score";
-$txt['OSUBJECT'] = "subject";
+$txt['OSUBJECT'] = "ämne";
 //old
-$txt['CRITERIAS'] = "Filter rules";
-$txt['FORCEMESSAGE'] = "Release the message";
-$txt['MESSAGEFORCING'] = "Message release";
+$txt['CRITERIAS'] = "Filterregler";
+$txt['FORCEMESSAGE'] = "Publicera meddelandet";
+$txt['MESSAGEFORCING'] = "Meddelandepublicering";
 $txt['MESSAGEPREVIEW'] = "Display the contents of the message";
-$txt['ANALYSEREQUEST'] = "Filter adjustment request";
-$txt['ANALYSEREQUESTV'] = "Request a filter adjustment";
-$txt['SCORETEXT'] = "Score: __PARAM__";
-$txt['CLOSEWINDOW'] = "Close the window";
-$txt['CURRENTPAGE'] = "Page __PARAM1__ of __PARAM2__";
-$txt['PURGEINFOS'] = "<strong>Automatic purge after:</strong> __PARAM__ days";
-$txt['DISPLAYEDINFOS'] = "<strong>Messages displayed: </strong> __PARAM1__ last days (<a href=\"__PARAM2__\">Modify...</a>). ";
-$txt['DISPLAYEDINFOSS'] = "<strong>Messages displayed: </strong> __PARAM__ last days. ";
+$txt['ANALYSEREQUEST'] = "Begäran om filterjustering";
+$txt['ANALYSEREQUESTV'] = "Begär en filterjustering";
+$txt['SCORETEXT'] = "Poäng: __PARAM__";
+$txt['CLOSEWINDOW'] = "Stäng fönstret";
+$txt['CURRENTPAGE'] = "Sida __PARAM1__ av __PARAM2__";
+$txt['PURGEINFOS'] = "<strong>Automatisk rensning efter:</strong> __PARAM__ dagar";
+$txt['DISPLAYEDINFOS'] = "<strong>Meddelande visade: </strong> __PARAM1__ senare dagarna (<a href=\"__PARAM2__\">Modifiera…</a>). ";
+$txt['DISPLAYEDINFOSS'] = "<strong>Meddelande visade: </strong> __PARAM__ senare dagarna. ";
 //old
-$txt['QUARANTINETITLE'] = "Quarantine list for __PARAM__";
+$txt['QUARANTINETITLE'] = "Karantänlista för __PARAM__";
 //old
 $txt['FILTER'] = "Filter";
 //old
-$txt['ADVANCEDSEARCH'] = "advanced search";
+$txt['ADVANCEDSEARCH'] = "avancerad sökning";
 //old
-$txt['SHOWEDMESSAGES'] = "displayed messages";
+$txt['SHOWEDMESSAGES'] = "visade meddelanden";
 //old
-$txt['COMINGFROM'] = "sender";
+$txt['COMINGFROM'] = "avsändare";
 $txt['DATE'] = "Date";
-$txt['HOUR'] = "Time";
-$txt['FROM'] = "From";
+$txt['HOUR'] = "Tid";
+$txt['FROM'] = "Från";
 $txt['TO'] = "To";
-$txt['SUBJECT'] = "Subject";
+$txt['SUBJECT'] = "Ämne";
 $txt['SENDER'] = "Sender";
-$txt['FORCED'] = "Released";
+$txt['FORCED'] = "Publicerat";
 $txt['ACTION'] = "Action";
 //old
-$txt['TOTALSPAMS'] = "Total: __PARAM__ spam(s)";
+$txt['TOTALSPAMS'] = "Totalt: __PARAM__ skräpmeddelande(n)";
 //old
-$txt['FORTHEXLASTDAYS'] = "__PARAM__ last days";
-$txt['PURGESELECTEDSPAMS'] = "Manually purge the quarantine";
+$txt['FORTHEXLASTDAYS'] = "__PARAM__ senaste dagarna";
+$txt['PURGESELECTEDSPAMS'] = "Rensa karantänen manuellt";
 //old
-$txt['HIDEFORCED'] = "Hide user-released messages";
+$txt['HIDEFORCED'] = "Göm användarrelaterade meddelanden";
 //old
-$txt['FORCESPAM'] = "release the message";
+$txt['FORCESPAM'] = "publicera meddelandet";
 //old
-$txt['ASKREASON'] = "display filtering rules";
-$txt['CONFSENDANALYSE'] = "A filter adjustment request will be sent to the MailCleaner Analysis Center; it includes a copy of the message that was wrongly blocked.";
-$txt['AREYOUSURE'] = "<span class=\"question\">Are you sure you want this message to be analyzed and that filter adjustments may be applied?</span>";
-$txt['ASKANALYSE'] = "Filter adjustment request";
+$txt['ASKREASON'] = "visa filtreringsregler";
+$txt['CONFSENDANALYSE'] = "En begäran om filterjustering kommer att skickas till MailCleaner Analysis Center; det inkluderar en kopia av meddelandet som felaktigt blockerats.";
+$txt['AREYOUSURE'] = "<span class=\"question\">Är du säker på att6 du vill att detta meddelande ska analyseras och att filterjusteringar kan tillämpas?</span>";
+$txt['ASKANALYSE'] = "Begäran om filterjustering";
 $txt['PAGE'] = "Page";
-$txt['NEXTPAGE'] = "Next";
-$txt['PREVIOUSPAGE'] = "Previous";
+$txt['NEXTPAGE'] = "Nästa";
+$txt['PREVIOUSPAGE'] = "Föregående";
 //old
-$txt['GETREASONSTITLE'] = "Filtering reasons";
-$txt['HITRULE'] = "Rule";
+$txt['GETREASONSTITLE'] = "Filtreringsorsaker";
+$txt['HITRULE'] = "Regel";
 $txt['SCORE'] = "Score";
 $txt['TOTAL'] = "Total";
 //old
-$txt['SENDANALYSETITLE'] = "Filter adjustment request";
-$txt['SENTTOANALYSE'] = "A filter adjustment request has been sent to the MailCleaner Analysis Center; this message will be examined and filter corrections may be applied within one working day.";
+$txt['SENDANALYSETITLE'] = "Begäran om filterjustering";
+$txt['SENTTOANALYSE'] = "En begäran om filterjustering har skickats till MailCleaner Analysis Center; detta meddelande kommer att undersökas och filterkorrigeringar kan komma att appliceras inom en arbetsdag.";
 /*
  * quick actions
  */
 //old
-$txt['SENDSUMTITLE'] = "Manually send the quarantine report";
-$txt['SENDSUM'] = "Manually send the quarantine report";
-$txt['SUMSENTTO'] = "The quarantine report has been sent to: <strong>__PARAM__</strong>";
-$txt['SUMNOTSENTTO'] = "The quarantine report could not be sent to <strong>__PARAM__</strong> because of an internal error. Please try again later.";
-$txt['SUMMARYSENDING'] = "Quarantine report transmission";
-$txt['PURGETITLE'] = "Manually purge the quarantine";
-$txt['PURGEDONE'] = "The quarantine for <strong>__PARAM__</strong> has been purged";
-$txt['COULDNOTPURGE'] = "The quarantine for <strong>__PARAM__</strong> could not be purged because of an internal error. Please try again later.";
-$txt['ASKPURGECONFIRM'] = "Are you sure you want to purge the last __PARAM1__ days of the quarantine for <strong>__PARAM2__</strong> ?";
-$txt['QUARANTINEPURGE'] = "Manual purge of the quarantine";
-$txt['GROUPQUARANTINES'] = "All addresses";
+$txt['SENDSUMTITLE'] = "Skicka karantänrapporten manuellt";
+$txt['SENDSUM'] = "Skicka karantänrapporten manuellt";
+$txt['SUMSENTTO'] = "Karantänrapporten har skickats till: <strong>__PARAM__</strong>";
+$txt['SUMNOTSENTTO'] = "Karantänrapporten kunde inte skickas till <strong>__PARAM__</strong> på grund av ett internt fel. Försök igen senare.";
+$txt['SUMMARYSENDING'] = "Överföring av karantänrapport";
+$txt['PURGETITLE'] = "Rensa karantänen manuellt";
+$txt['PURGEDONE'] = "Karantänen för <strong>__PARAM__</strong> har rensats";
+$txt['COULDNOTPURGE'] = "Karantänen för <strong>__PARAM__</strong> kunde inte rensas på grund av ett internt fel. Försök igen senare.";
+$txt['ASKPURGECONFIRM'] = "Är du säker på att du vill rensa de senaste __PARAM1__ dagarnas karantän för <strong>__PARAM2__</strong> ?";
+$txt['QUARANTINEPURGE'] = "Manuell rensning av karantänen";
+$txt['GROUPQUARANTINES'] = "Alla adresser";
 /*
  * preview panel
  */
-$txt['INFORMATIONSABOUTMSG'] = "Contents of the message";
+$txt['INFORMATIONSABOUTMSG'] = "Meddelandets innehåll";
 $txt['PREFILTERHITS'] = "Pre-filters";
-$txt['BLACKLISTS'] = "Blacklists";
+$txt['BLACKLISTS'] = "Svartlistor";
 $txt['FITLERSCORE'] = "Score";
 $txt['NONE'] = "none";
-$txt['HEADERS'] = "Headers";
-$txt['BODY'] = "Body";
-$txt['PARTS'] = "Parts";
-$txt['STORESLAVE'] = "Storage";
-$txt['SPAMCSCORE'] = "Rules score";
+$txt['HEADERS'] = "Huvuden";
+$txt['BODY'] = "Kropp";
+$txt['PARTS'] = "Delar";
+$txt['STORESLAVE'] = "Lagring";
+$txt['SPAMCSCORE'] = "Regelpoäng";
 $txt['DESCRIPTION'] = "Description";
 $txt['MESSAGEPREVIEW'] = "Förhandsvisning av meddelande";
-$txt['ID'] = "Identifier";
+$txt['ID'] = "Identifierare";
 /*
  * logout page
  */
-$txt['LOGOUTTEXT'] = "<p>Your MailCleaner session ended.</p><p>If you would like to start a new session, click on the following link:</p>";
+$txt['LOGOUTTEXT'] = "<p>Din MailCleaner-session avslutades.</p><p>Om du skulle vilja start en ny session, klicka på följande länk:</p>";
 /*
  * parameters page
  */
 //old
-$txt['PARAMTITLE'] = "Personal MailCleaner settings";
+$txt['PARAMTITLE'] = "Personliga MailCleaner-inställningar";
 //old
-$txt['USERPARAM'] = "User settings";
+$txt['USERPARAM'] = "Användarinställningar";
 //old
-$txt['ADDRESSPARAM'] = "Address settings";
+$txt['ADDRESSPARAM'] = "Adressinställningar";
 //old
-$txt['LANGUAGE'] = "Language";
+$txt['LANGUAGE'] = "Språk";
 $txt['ADDRESS'] = "Address";
 //old
-$txt['ADDRALIASTITLE'] = "Address group";
+$txt['ADDRALIASTITLE'] = "Adressgrupp";
 //old
-$txt['FILTERACTIONTITLE'] = "Spam processing mode";
+$txt['FILTERACTIONTITLE'] = "Läge för hantering av skräppost";
 //old
-$txt['SUMMARYPARAMSTITLE'] = "Quarantine report settings";
+$txt['SUMMARYPARAMSTITLE'] = "Karantänrapportinställningar";
 //old
-$txt['ADDRESSESLISTTITLE'] = "Personal address group: (__PARAM__ address(es))";
+$txt['ADDRESSESLISTTITLE'] = "Personlig adressgrupp: (__PARAM__ adress(er))";
 //old
-$txt['APPLYALLADDRESSES'] = "Apply changes to all addresses";
+$txt['APPLYALLADDRESSES'] = "Verkställ ändringar för alla adresser";
 //old
-$txt['MAIN'] = "main";
+$txt['MAIN'] = "huvud";
 //old
-$txt['SPAMACTION'] = "Spam processing mode";
+$txt['SPAMACTION'] = "Läget för hantering av skräppost";
 //old
-$txt['PUTINQUARANTINE'] = "quarantine";
+$txt['PUTINQUARANTINE'] = "karantän";
 //old
 $txt['TAGSUBJECT'] = "tag";
 //old
-$txt['QUARBOUNCES'] = "Systematically quarantine e-mail delivery error messages";
+$txt['QUARBOUNCES'] = "Sätt systematiskt leveransfelmeddelanden för e-post i karantän";
 //old
-$txt['DROP'] = "drop";
+$txt['DROP'] = "släng";
 //old
-$txt['SUBJECTTAG'] = "Tag keyword";
+$txt['SUBJECTTAG'] = "Tagg-nyckelord";
 //old
-$txt['SUMMARYFREQ'] = "Report delivery frequency";
-$txt['DAILY'] = "Daily";
-$txt['WEEKLY'] = "Weekly";
-$txt['MONTHLY'] = "Monthly";
+$txt['SUMMARYFREQ'] = "Leveransfrekvens för rapport";
+$txt['DAILY'] = "Dagligen";
+$txt['WEEKLY'] = "Varje vecka";
+$txt['MONTHLY'] = "Månatligen";
 $txt['NOSUMMARY'] = "No report";
 //old
 $txt['SUMMARYTYPE'] = "Report format";
-$txt['USEDEFAULT'] = "use default";
+$txt['USEDEFAULT'] = "använd standard";
 $txt['SUMMHTML'] = "html";
-$txt['SUMMTEXT'] = "plain text";
+$txt['SUMMTEXT'] = "vanlig text";
 //old
-$txt['EDITWHITELIST'] = "edit white list";
+$txt['EDITWHITELIST'] = "redigera vitlista";
 //old
-$txt['EDITWARNLIST'] = "edit warn list";
+$txt['EDITWARNLIST'] = "redigera varningslista";
 /*
  * wwlist management
  */
 //old
-$txt['SENDER'] = "Sender";
-$txt['COMMENT'] = "Comment";
+$txt['SENDER'] = "Avsändare";
+$txt['COMMENT'] = "Kommentera";
 //old
 $txt['ACTIONS'] = "Action";
 //old
 $txt['ACTIVE'] = "active";
 //old
-$txt['INNACTIVE'] = "inactive";
+$txt['INNACTIVE'] = "inaktiv";
 //old
-$txt['WWENTRY'] = "Entry";
+$txt['WWENTRY'] = "Post";
 //old
-$txt['CONFIRMWWENTRYDELETE'] = "Are you sure you want to permanently delete this entry?";
+$txt['CONFIRMWWENTRYDELETE'] = "Är du säker på att du vill ta bort denna post permanent?";
 //old
-$txt['WHITELISTFOR'] = "White list for __PARAM__";
+$txt['WHITELISTFOR'] = "Vitlista för __PARAM__";
 //old
-$txt['WARNLISTFOR'] = "Warn list for __PARAM__";
+$txt['WARNLISTFOR'] = "Varningslista för __PARAM__";
 //old
 $txt['STATUS'] = "Status";
 /*
  * alias stuff
  */
 //old
-$txt['ADDALIASTITLE'] = "Add an address";
+$txt['ADDALIASTITLE'] = "Lägg till en adress";
 //old
 $txt['ADDALIASFORM'] = "Address";
 //old
-$txt['REMADDRESSALT'] = "remove an address";
+$txt['REMADDRESSALT'] = "ta bort en adress";
 //old
-$txt['ADDADDRESSALT'] = "add an address";
-$txt['BADADDRESSFORMAT'] = "The given address is invalid. Please try again.";
-$txt['NOTFILTEREDDOMAIN'] = "This domain is not filtered by MailCleaner. The address could not be added.";
-$txt['ALIASALREADYREGISTERD'] = "This address is already used by a MailCleaner account. It could not be added.";
-$txt['ALIASALREADYPENDING'] = "A confirmation is already pending for this address.";
-$txt['ALIASPENDING'] = "A confirmation has been sent to this address.<br/> For security reasons, the address will only be added if you confirm the request within 24 hours.";
+$txt['ADDADDRESSALT'] = "lägg till en adress";
+$txt['BADADDRESSFORMAT'] = "Den angivna adressen är ogiltig. Försök igen.";
+$txt['NOTFILTEREDDOMAIN'] = "Denna domän är inte filtrerad av MailCleaner. Adressen kunde inte läggas till.";
+$txt['ALIASALREADYREGISTERD'] = "Denna adress är redan använd av ett MailCleaner-konto. Den kunde inte läggas till.";
+$txt['ALIASALREADYPENDING'] = "En bekräftelse är redan utskickad för denna adress.";
+$txt['ALIASPENDING'] = "En bekräftelse har skickats till denna adress.<br/> Av säkerhetsskäl kommer adressen endast att läggas till om du bekräftar begäran inom 24 timmar.";
 $txt['ALIASREQUESTSUBJECT'] = "[MailCleaner] Address addition request";
 $txt['ALIASERRORSENDIG'] = "The request could not be sent because of an internal error. Please try again later.";
 $txt['ALIASNOTPENDING'] = "No request is currently pending for this address.";
