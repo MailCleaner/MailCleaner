affebd5644a044454fc155db1f521101227f96b9
Marin Gilles <mrngilles@gmail.com>
2018-11-08 11:08:44 +0100

    Change key cipher to RSA for internal access

--- install/dbs/t_cf_internal_access.sql
+++ install/dbs/t_cf_internal_access.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS internal_access;
 CREATE TABLE internal_access (
     id INT(10) NOT NULL AUTO_INCREMENT,
     time_created TIMESTAMP DEFAULT NOW(),
-    private_key VARCHAR(400) NOT NULL,
-    public_key VARCHAR(300) NOT NULL,
+    private_key VARCHAR(4000) NOT NULL,
+    public_key VARCHAR(1000) NOT NULL,
     PRIMARY KEY (id)
 );
