1aecead914b9a570899af00dcc64067717983cce
p-rolle <squale@gmail.com>
2019-02-15 10:01:10 +0100

    noexit for fetch_ scripts so that logs or services restarts are made

--- bin/fetch_clamspam.sh
+++ bin/fetch_clamspam.sh
@@ -73,7 +73,7 @@ fi
 
 . $SRCDIR/lib/updates/download_files.sh
 
-downloadDatas "$VARDIR/spool/clamspam/" "clamspam3" $randomize "clamav" "\|local_whitelist.ign2"
+downloadDatas "$VARDIR/spool/clamspam/" "clamspam3" $randomize "clamav" "\|local_whitelist.ign2" "noexit"
 
 ## restart clamspam daemon
 kill -USR2 `cat $VARDIR/run/clamav/clamspamd.pid 2>/dev/null` > /dev/null 2>&1
