660a16e630126b18ca3b74fefe91449e0f9003b8
Daniel Jorge <jorge.daniel@protonmail.com>
2020-04-26 16:48:27 +0200

    Specifying python version in script, added header in file

--- install/install_pyenv_3-7-7.sh
+++ install/install_pyenv_3-7-7.sh
@@ -1,4 +1,27 @@
 #! /bin/bash
+#
+#   Mailcleaner - SMTP Antivirus/Antispam Gateway
+#   Copyright (C) 2004-2014 Olivier Diserens <olivier@diserens.ch>
+#   Copyright (C) 2015-2017 Mentor Reka <reka.mentor@gmail.com>
+#   Copyright (C) 2015-2017 Florian Billebault <florian.billebault@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
+#   This script will install pyenv, python 3.7.7 and MailCleaner library
+#
 VARDIR="/var/mailcleaner"
 cd $VARDIR
 
