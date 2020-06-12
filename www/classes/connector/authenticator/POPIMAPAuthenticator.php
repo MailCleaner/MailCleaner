1935a9e705a141ac583ff98e484b6feaec96d29f
Pascal Rolle <squale@gmail.com>
2020-06-12 18:04:09 +0200

    Correctly close imap connection

--- www/classes/connector/authenticator/POPIMAPAuthenticator.php
+++ www/classes/connector/authenticator/POPIMAPAuthenticator.php
@@ -77,7 +77,7 @@ class POPIMAPAuthenticator extends AuthManager {
        $res = imap_open($dn, $username, $_POST['password']);
                     
        if ($res) {
-	  imap_close($res);
+          imap_close($res);
        	  $this->status_ = 0;
        	  return true;
        }
