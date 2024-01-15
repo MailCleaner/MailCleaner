04bedc0c5669bebf1beeae0af549e61708cce4c3
John Mertz <git@john.me.tz>
2024-01-15 13:38:04 -0500

    Prep work for Exim 4.97.1
    Exim IDs have become much longer and we have 16 characters hard-coded in several places.
    
    Also requires new template files. These are direct copies of the 4.96 versions, except stage4, which removes the MySQL stub. This may be restored if I successfully build the library for this version.
    
    Still pending an Updater4MC to upgrade the DB column and install 4.97.1, once uploaded to the repo.
    
    Will also require more testing.

--- bin/search_log.pl
+++ bin/search_log.pl
@@ -203,7 +203,7 @@ if (@messages > 0) {
         print FULLOG $msg_o{'nid'}.'|' if ($batchwithlog);
         print FULLOG $line;
         print FULLOG "\n";
-        if ($line =~ m/ready to be delivered with new id: (\S{16})/) {
+        if ($line =~ m/ready to be delivered with new id: (\S{16,24})/) {
         	$out_id = $1;
         }
       }
@@ -258,15 +258,15 @@ sub populateIDs {
     next if ($line !~ /^(\d\d\d\d)-(\d\d)-(\d\d)/);
     my $date = "$1$2$3";
     next if $date < $start;
-    next if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d) \d\d:\d\d:\d\d\ (\S{16} )?DEBUG /);
+    next if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d) \d\d:\d\d:\d\d\ (\S{16,24} )?DEBUG /);
     next if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d) \d\d:\d\d:\d\d\ Authentication passed/);
     next if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d) \d\d:\d\d:\d\d\ Accepting authenticated/);
     next if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d) \d\d:\d\d:\d\d\ Accepting authorized relaying/);
     next if ($line =~ /Warning: Doing LDAP verification for/);
-    if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d)\ \d\d:\d\d:\d\d\ ([-a-zA-Z0-9]{16})\ /) {
+    if ($line =~ /^(\d\d\d\d)-(\d\d)-(\d\d)\ \d\d:\d\d:\d\d\ ([-a-zA-Z0-9]{16,24})\ /) {
       my $id = $4;
       my $nid = '';
-      if ($line =~ /250 OK id=(\S{16})/ && $line !~ /T=remote_smtp/) {
+      if ($line =~ /250 OK id=(\S{16,24})/ && $line !~ /T=remote_smtp/) {
         $nid = $1; 
         $internal_messages{$nid} = 1;
       } 
@@ -336,7 +336,7 @@ sub searchExim {
   }
 
   while (<$fh>) {
-    if (/^(\d\d\d\d)-(\d\d)-(\d\d)\ \d\d:\d\d:\d\d\ (\S{16})\ /) {
+    if (/^(\d\d\d\d)-(\d\d)-(\d\d)\ \d\d:\d\d:\d\d\ (\S{16,24})\ /) {
      if (defined($internal_messages{$4}) ) {
        $store->{$4} .= $_;
      }
@@ -361,7 +361,7 @@ sub searchMailScanner {
   }
  
   while (<$fh>) {
-    if (/\b(\S{6}-\S{6}-\S{2})\b/) {
+    if (/\b(\S{6}-\S{6,11}-\S{2,4})\b/) {
       if (defined($internal_messages{$1}) ) {
        $store->{$1} .= $_;
      }
@@ -387,10 +387,10 @@ sub searchSpamHandler {
   }
 
   while (<$fh>) {
-    if (/^(\d\d\d\d)-(\d\d)-(\d\d)\ \d\d:\d\d:\d\d\ \(\d+\)\ \d+: message (\S{16})/) {
+    if (/^(\d\d\d\d)-(\d\d)-(\d\d)\ \d\d:\d\d:\d\d\ \(\d+\)\ \d+: message (\S{16,24})/) {
      if (defined($internal_messages{$4}) ) {
        $store->{$4} .= $_;
-       if (/ready to be delivered with new id: (\S{16})/) {
+       if (/ready to be delivered with new id: (\S{16,24})/) {
        	  $internal_messages{$1} = 1;
        }
      }
@@ -627,7 +627,7 @@ sub processStage4Logs {
         	if ($shline =~ m/^(\d{4}-\d\d-\d\d\ \d\d:\d\d:\d\d)/ ) {
         		 $date = $1;
         	}
-        	if ($shline =~ /ready to be delivered with new id: (\S{16})/) {
+        	if ($shline =~ /ready to be delivered with new id: (\S{16,24})/) {
         		($dateout, $delivered, $outreport, $outmessage, $outhost) = processStage4Logs($1);
         		$outdateset = 1;
         	}
