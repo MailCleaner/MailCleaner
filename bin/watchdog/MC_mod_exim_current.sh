f258aa7a170d7eafd1f22016d6099b9f9e86fc2f
John Mertz <git@john.me.tz>
2024-02-13 13:38:28 -0700

    Update watchdog for 4.97.1 (#583)

--- bin/watchdog/MC_mod_exim_current.sh
+++ bin/watchdog/MC_mod_exim_current.sh
@@ -36,9 +36,9 @@ then
     EXIT=1
 fi
 
-if [[ $(dpkg -l |grep mc-exim | sed -e 's/.*4.96.*/4.96/') != '4.96' ]]
+if [[ $(dpkg -l |grep mc-exim | sed -e 's/.*4\.97\.1.*/4.97.1/') != '4.97.1' ]]
 then
-    echo "mc_exim is not in version 4.96" >> $OUT_FILE
+    echo "mc_exim is not in version 4.97" >> $OUT_FILE
     EXIT=1
 fi
 
