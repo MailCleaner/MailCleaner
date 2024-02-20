f92f79e6078823c9f5de37cc7ad18fd7a3afdb3b
John Mertz <git@john.me.tz>
2024-02-19 19:16:07 -0500

    Prioritize Fail2Ban check

--- scripts/cron/mailcleaner_cron.pl
+++ scripts/cron/mailcleaner_cron.pl
@@ -67,6 +67,29 @@ if (defined $options{h}) {
   usage();
 }
 
+############################################
+# Restart Fail2Ban if inaccessible/stopped #
+############################################
+unless ( -e "$config{'VARDIR'}/run/fail2ban.disabled" ) {
+  my $timout = 5;
+  my $failed = 0;
+  my $cmd = "/usr/bin/fail2ban-client status 2>/dev/null";
+  eval {
+    local $SIG{ALRM} = sub { die "timeout\n" };
+    alarm $timout;
+    system($cmd);
+    $failed=1 if ($?);
+    alarm 0;
+  };
+  if ($@) {
+    print("fail2ban not responding, restarting firewall...\n");
+    system("$config{'SRCDIR'}/etc/init.d/firewall restart");
+  } elsif ($failed) {
+    print("fail2ban not running, restarting firewall...\n");
+    system("$config{'SRCDIR'}/etc/init.d/firewall restart");
+  }
+}
+
 ###########################
 # We need the DB to be okay to make sure we are dropping
 # the right informations in configuration files
@@ -651,29 +674,6 @@ while (scalar(@remaining)) {
     if (scalar(@remaining) == scalar(@wait)) {
       last;
     }
-  }
-}
-
-############################################
-# Restart Fail2Ban if inaccessible/stopped #
-############################################
-unless ( -e "$config{'VARDIR'}/run/fail2ban.disabled" ) {
-  my $timout = 5;
-  my $failed = 0;
-  my $cmd = "/usr/bin/fail2ban-client status 2>/dev/null";
-  eval {
-    local $SIG{ALRM} = sub { die "timeout\n" };
-    alarm $timout;
-    system($cmd);
-    $failed=1 if ($?);
-    alarm 0;
-  };
-  if ($@) {
-    print("fail2ban not responding, restarting firewall...\n");
-    system("$config{'SRCDIR'}/etc/init.d/firewall restart");
-  } elsif ($failed) {
-    print("fail2ban not running, restarting firewall...\n");
-    system("$config{'SRCDIR'}/etc/init.d/firewall restart");
   }
 }
 
