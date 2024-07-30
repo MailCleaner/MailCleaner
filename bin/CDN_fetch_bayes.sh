b44a48caa95e98f3efc9dc928e07dc3ad3d3582f
John Mertz <git@john.me.tz>
2024-07-29 23:44:30 -0400

    Completely rework Bayes fetcher
    Needed to change for AWS as CDN.
    
    Rather than Enterprise users downloading HTTP credentials, it now downloads a secret (the md5 hash of the target file). If secrets are downloaded, that has is used to locate the target file, otherwise (Community edition) it will download a monthly snapshot from a public path.
    
    Accompanies changes to infrastructure scripts.

--- bin/CDN_fetch_bayes.sh
+++ bin/CDN_fetch_bayes.sh
@@ -38,114 +38,123 @@ LOGFILE="$VARDIR/log/mailcleaner/downloadDatas.log"
 
 function log {
     echo "["`date "+%Y/%m/%d %H:%M:%S"`"] $1" >> $LOGFILE
+    if [ ! -z $2 ]; then
+        removeLockFile "$FILE_NAME"
+        exit $2
+    fi
 }
 
 FILE_NAME=$(basename -- "$0")
 FILE_NAME="${FILE_NAME%.*}"
 ret=$(createLockFile "$FILE_NAME")
 if [[ "$ret" -eq "1" ]]; then
-        exit 0
+    log "Lockfile exists for $FILE_NAME" 0
 fi
 
 if [ !  -d $VARDIR/spool/data_credentials/ ]; then
-	mkdir -p $VARDIR/spool/data_credentials/
+    mkdir -p $VARDIR/spool/data_credentials/
 fi
 
 # If the bayesian doesnt exist as a file, we remove its md5 associated file
 # That way the re download will be forced
 if [ ! -f $VARDIR/spool/spamassassin/tmp/bayes_toks ]; then
-	rm -f $VARDIR/spool/spamassassin/bayes_toks.md5
+    rm -f $VARDIR/spool/spamassassin/bayes_toks.md5
 fi
 if [ ! -f $VARDIR/spool/bogofilter/database/wordlist.db ]; then
-        rm -f $VARDIR/spool/bogofilter/database/wordlist.db.md5 > /dev/null 2>&1
+    rm -f $VARDIR/spool/bogofilter/database/wordlist.db.md5 > /dev/null 2>&1
+fi
+# Remove cached secrets
+if [ -f /tmp/bayes_secrets ]; then
+    rm -f /tmp/bayes_secrets > /dev/null 2>&1
 fi
 
-# Getting credentials
-rm -f /tmp/data_credentials > /dev/null 2>&1
-scp -q -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no mcscp@cvs.mailcleaner.net:/data_credentials/data_credentials /tmp/data_credentials
-if [ -f /tmp/data_credentials ]; then
-	NEW_CRED=`cat /tmp/data_credentials`
+# Getting secrets
+scp -q -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no mcscp@cvs.mailcleaner.net:/data_credentials/bayes_secrets /tmp/bayes_secrets
+
+OLD_SA_SECRET=''
+OLD_BOGO_SECRET=''
+if [ -f $VARDIR/spool/data_credentials/bayes_secrets ]; then
+    OLD_SA_SECRET=`grep spamassassin $VARDIR/spool/data_credentials/bayes_secrets | cut -d ' ' -f 2`
+    OLD_BOGO_SECRET=`grep bogofilter $VARDIR/spool/data_credentials/bayes_secrets | cut -d ' ' -f 2`
+fi
+
+# Enterprise Edition
+if [ -f /tmp/bayes_secrets ]; then
+    SA_SECRET=`grep spamassassin /tmp/bayes_secrets | cut -d ' ' -f 2`
+    BOGO_SECRET=`grep bogofilter /tmp/bayes_secrets | cut -d ' ' -f 2`
+    if [[ $OLD_SA_SECRET == $SA_SECRET ]] && [[ $OLD_BOGO_SECRET == $BOGO_SECRET ]]; then
+        log "Enterprise data is already current" 0
+    fi
+
+    wget http://cdnpush.mailcleaner.net.s3.amazonaws.com/bayes_toks_$SA_SECRET -P /tmp/ > /dev/null 2>&1
+    if [[ "$SA_SECRET" -eq "`md5sum /tmp/bayes_toks_$SA_SECRET | cut -d ' ' -f 2`" ]]; then
+        mv -f /tmp/bayes_toks_$SA_SECRET $VARDIR/spool/spamassassin/ > /dev/null 2>&1
+    fi
+
+    wget http://cdnpush.mailcleaner.net.s3.amazonaws.com/wordlist.db_$BOGO_SECRET -P /tmp/ > /dev/null 2>&1
+    if [[ "$BOGO_SECRET" -eq "`md5sum /tmp/wordlist.db_$BOGO_SECRET | cut -d ' ' -f 2`" ]]; then
+        mv -f /tmp/wordlist.db_$BOGO_SECRET $VARDIR/spool/spamassassin/ > /dev/null 2>&1
+    fi
+
+    mv -f /tmp/bayes_secrets $VARDIR/spool/data_credentials/bayes_secrets
+    log "Updated with latest Enterprise data"
+
+# Community Edition snapshots
 else
-	log "Couldnt get credentials file"
-	exit 1
-fi
-if [ -f $VARDIR/spool/data_credentials/data_credentials ]; then
-	CURRENT_CRED=`cat $VARDIR/spool/data_credentials/data_credentials`
-else
-	CURRENT_CRED=0
-fi
+    # Spamassassin
+    rm -f /tmp/bayes_toks.md5 > /dev/null 2>&1
+    wget http://cdnpush.mailcleaner.net.s3.amazonaws.com/bayes_toks.md5 -P /tmp/ > /dev/null 2>&1
+    if [ ! -f /tmp/bayes_toks.md5 ]; then
+        log "Could not retrieve bayes_toks.md5" 1
+    fi
+    NEW_MD5=`cut -d' ' -f1 /tmp/bayes_toks.md5`
+    if [ -f $VARDIR/spool/spamassassin/bayes_toks.md5 ]; then
+        CURRENT_MD5=`cut -d' ' -f1 $VARDIR/spool/spamassassin/bayes_toks.md5`
+    else
+        CURRENT_MD5=0
+    fi
 
-# If credentials changed, it is time for new bayesians
-if [[ "$CURRENT_CRED" != "$NEW_CRED" ]]; then
-	USER=`cat /tmp/data_credentials |cut -d' ' -f1`;
-	PASS=`cat /tmp/data_credentials |cut -d' ' -f2`;
-
-# Updating Bayes_toks
-	# Getting MD5
-	rm -f /tmp/bayes_toks.md5 > /dev/null 2>&1
-	wget --user=$USER --password=$PASS https://cdnpush.s3.us-east-2.stackpathstorage.com/bayesdb/bayes_toks.md5 -P /tmp/ > /dev/null 2>&1
-	if [ ! -f /tmp/bayes_toks.md5 ]; then
-		log "Could not retrieve bayes_toks.md5"
-		exit 1
-	fi
-	NEW_MD5=`cut -d' ' -f1 /tmp/bayes_toks.md5`
-	if [ -f $VARDIR/spool/spamassassin/bayes_toks.md5 ]; then
-		CURRENT_MD5=`cut -d' ' -f1 $VARDIR/spool/spamassassin/bayes_toks.md5`
-	else
-		CURRENT_MD5=0
-	fi
-
-	# If MD5 changed then the associated bayesians have to be updated
-	if [[ "$CURRENT_MD5" != "$NEW_MD5" ]]; then
-		wget --user=$USER --password=$PASS https://cdnpush.s3.us-east-2.stackpathstorage.com/bayesdb/bayes_toks -P /tmp/ > /dev/null 2>&1
-		if [ ! -f /tmp/bayes_toks ]; then
-			log "Could not retrieve bayes_toks"
-			exit 1
-		fi
-		mv -f /tmp/bayes_toks $VARDIR/spool/spamassassin/ > /dev/null 2>&1
-		mv -f /tmp/bayes_toks.md5 $VARDIR/spool/spamassassin/ > /dev/null 2>&1
-		log "updating bayes_toks"
-	else
-		log "No new bayes_toks (md5 didnt change)"
-	fi
-
-# Updating wordlist.db
-        # Getting MD5
-        rm -f /tmp/wordlist.db.md5 > /dev/null 2>&1
-        wget --user=$USER --password=$PASS https://cdnpush.s3.us-east-2.stackpathstorage.com/bayesdb/wordlist.db.md5 -P /tmp/ > /dev/null 2>&1
-        if [ ! -f /tmp/wordlist.db.md5 ]; then
-                log "Could not retrieve wordlist.db.md5"
-                exit 1
+    if [[ "$CURRENT_MD5" != "$NEW_MD5" ]]; then
+        wget https://cdnpush.mailcleaner.net.s3.amazonaws.com/bayes_toks -P /tmp/ > /dev/null 2>&1
+        if [ ! -f /tmp/bayes_toks ]; then
+            log "Could not retrieve bayes_toks" 1
         fi
-        NEW_MD5=`cut -d' ' -f1 /tmp/wordlist.db.md5`
-        if [ -f $VARDIR/spool/bogofilter/database/wordlist.db.md5 ]; then
-                CURRENT_MD5=`cut -d' ' -f1 $VARDIR/spool/bogofilter/database/wordlist.db.md5`
-        else
-                CURRENT_MD5=0
+        mv -f /tmp/bayes_toks $VARDIR/spool/spamassassin/ > /dev/null 2>&1
+        mv -f /tmp/bayes_toks.md5 $VARDIR/spool/spamassassin/ > /dev/null 2>&1
+        log "Updated with latest Community snapshot of bayes_toks"
+    else
+        log "No new bayes_toks (md5 didnt change)"
+    fi
+    
+    # Bogofilter
+    rm -f /tmp/wordlist.db.md5 > /dev/null 2>&1
+    wget https://cdnpush.mailcleaner.net.s3.amazonaws.com/wordlist.db.md5 -P /tmp/ > /dev/null 2>&1
+    if [ ! -f /tmp/wordlist.db.md5 ]; then
+        log "Could not retrieve wordlist.db.md5" 1
+    fi
+    NEW_MD5=`cut -d' ' -f1 /tmp/wordlist.db.md5`
+    if [ -f $VARDIR/spool/bogofilter/database/wordlist.db.md5 ]; then
+        CURRENT_MD5=`cut -d' ' -f1 $VARDIR/spool/bogofilter/database/wordlist.db.md5`
+    else
+        CURRENT_MD5=0
+    fi
+
+    # If MD5 changed then the associated bayesians have to be updated
+    if [[ "$CURRENT_MD5" != "$NEW_MD5" ]]; then
+        wget https://cdnpush.mailcleaner.net.s3.amazonaws.com/$SECRET/wordlist.db -P /tmp/ > /dev/null 2>&1
+        if [ ! -f /tmp/wordlist.db ]; then
+            log "Could not retrieve wordlist.db" 1
         fi
+        mv -f /tmp/wordlist.db $VARDIR/spool/bogofilter/database/ > /dev/null 2>&1
+        mv -f /tmp/wordlist.db.md5 $VARDIR/spool/bogofilter/database/ > /dev/null 2>&1
+        log "Updated with latest Community snapshot of wordlist.db"
+    else
+        log "No new wordlist.db (md5 didnt change)"
+    fi
 
-        # If MD5 changed then the associated bayesians have to be updated
-        if [[ "$CURRENT_MD5" != "$NEW_MD5" ]]; then
-                wget --user=$USER --password=$PASS https://cdnpush.s3.us-east-2.stackpathstorage.com/bayesdb/wordlist.db -P /tmp/ > /dev/null 2>&1
-                if [ ! -f /tmp/wordlist.db ]; then
-                        log "Could not retrieve wordlist.db"
-                        exit 1
-                fi
-                mv -f /tmp/wordlist.db $VARDIR/spool/bogofilter/database/ > /dev/null 2>&1
-                mv -f /tmp/wordlist.db.md5 $VARDIR/spool/bogofilter/database/ > /dev/null 2>&1
-                log "updating wordlist.db"
-        else
-                log "No new wordlist.db (md5 didnt change)"
-        fi
-
-
-	mv -f /tmp/data_credentials $VARDIR/spool/data_credentials/data_credentials
-else
-	log "No new bayesian (credentials didnt change)"
+    rm -f /tmp/bayes_toks.md5 > /dev/null 2>&1
+    rm -f /tmp/wordlist.db.md5 > /dev/null 2>&1
 fi
-rm -f /tmp/bayes_toks.md5 > /dev/null 2>&1
-rm -f /tmp/wordlist.db.md5 > /dev/null 2>&1
-rm -f /tmp/data_credentials
 
 removeLockFile "$FILE_NAME"
 
